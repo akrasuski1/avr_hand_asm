f3e15346682bafc1b628f6143770007f57841a5e
Adam <akrasuski1@wp.pl>
2017-04-07 02:55:08 +0200

    Prettified check.sh

--- check.sh
+++ check.sh
@@ -2,9 +2,10 @@ gcc compress.c -o compress
 ./compress > compressed.h
 
 gcc disasm.c -o disas
-./disas > out
-diff <(nl out) <(nl avr_as)
+diff <(./disas | nl) <(nl avr_as)
 
-avr-gcc -c disasm.c -I"/usr/lib/avr/include" -mmcu=atmega328p -Os -g3 -Wall -Wextra -ffunction-sections -fdata-sections -fpack-struct -fshort-enums -funsigned-char -funsigned-bitfields -MMD -MP -DF_CPU=8000000UL -std=c11 -o main.o
-avr-gcc -mmcu=atmega328p -Wl,--gc-sections  -fno-exceptions -fno-rtti -o avr.elf main.o
+avr-gcc disasm.c -I"/usr/lib/avr/include" -mmcu=atmega328p -Os -g3 -Wall \
+	-Wextra -ffunction-sections -fdata-sections -fpack-struct -fshort-enums \
+	-funsigned-char -funsigned-bitfields -DF_CPU=8000000UL -std=c11 \
+	-o avr.elf -Wl,--gc-sections
 avr-size --format=avr --mcu=atmega328p avr.elf
